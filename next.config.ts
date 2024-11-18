21354bad0b7a02e1882f808d98216684deb192c7
Rasaan Mills <37400373-saanmills7@users.noreply.replit.com>
2024-11-18 19:05:09 +0000

    Updated next.config.ts and removed basePath for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/OccBean',
   reactStrictMode: true,
   trailingSlash: true,
   images: {
