e1db69f876a62d6bc51c8b6d8a44f5d36f4e0df6
Rasaan Mills <37400373-saanmills7@users.noreply.replit.com>
2024-11-18 20:04:12 +0000

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Home of the Beans',
-  description: 'Enter a world of digital chaos and imagination',
+  title: '🫘',
+  description: 'mmmmmm',
 }
 
 export default function RootLayout({
