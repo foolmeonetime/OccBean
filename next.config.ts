45b9d8ea17382b9b268c92ba89dd508206eeac50
Rasaan Mills <37400373-saanmills7@users.noreply.replit.com>
2024-11-18 15:50:30 +0000

    update moar

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+<<<<<<< HEAD
   output: 'export', // Enables static export
   basePath: '/OccBean', // Ensure this matches your GitHub repository name exactly (case-sensitive)
   images: {
@@ -10,8 +11,22 @@ const nextConfig: NextConfig = {
   trailingSlash: true, // Optional, but useful for GitHub Pages routing
   webpack(config) {
     config.resolve.alias['@'] = path.join(__dirname); // Map @ to project root (if you're using aliases)
+=======
+  output: 'export',
+  basePath: '/OccBean', 
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  webpack(config) {
+    config.resolve.alias['@'] = path.join(__dirname);
+>>>>>>> e85d0a7 (Updated project configuration and fixed issues)
     return config;
   },
 };
 
+<<<<<<< HEAD
 export default nextConfig;
+=======
+export default nextConfig;
+>>>>>>> e85d0a7 (Updated project configuration and fixed issues)
