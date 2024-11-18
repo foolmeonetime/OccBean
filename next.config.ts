d88439a49d5478ebaed2cc0b5103a158ae7ee1b5
Rasaan Mills <37400373-saanmills7@users.noreply.replit.com>
2024-11-18 16:19:15 +0000

    fix and merge config

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,26 @@
-  import path from 'path';
-  import type { NextConfig } from 'next';
+import path from 'path';
+import type { NextConfig } from 'next';
 
-  const nextConfig: NextConfig = {
-    output: 'export',
-    basePath: '/OccBean',
-    images: {
-      unoptimized: true,
-    },
-    trailingSlash: true,
-    webpack(config) {
-      config.resolve.alias['@'] = path.join(__dirname);
-      return config;
-    },
-  };
+const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/OccBean',
+  reactStrictMode: true,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack(config) {
+    config.resolve.alias['@'] = path.join(__dirname);
+    return config;
+  },
+};
 
-  export default nextConfig;
+export default nextConfig;
